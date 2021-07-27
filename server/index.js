5b375310f4c0dab279b5bd07e72f16337651f454
Levi McCormick <levimccormick34@gmail.com>
2021-07-27 10:21:09 -0600

    server update

--- server/index.js
+++ server/index.js
@@ -9,8 +9,13 @@ app.get('/', (req,res) => {
   res.sendFile(path.join(__dirname, 'public'));
 });
 
+app.get("/api.yelp.com/v3/businesses/search?term=delis&latitude=37.786882&longitude=-122.399972", (req, res) => {
+  console.log(res.data)
+})
+
 const port = process.env.PORT || 4005
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
 })
+
