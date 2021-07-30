4c292f9853e0229c801bc7b557a2bf8e82512afe
Levi McCormick <levimccormick34@gmail.com>
2021-07-29 21:43:59 -0600

    update

--- public/index.js
+++ public/index.js
@@ -5,19 +5,19 @@ const price = document.querySelector('#price')
 const businessContainer = document.querySelector('#call-container')
 const reset = document.querySelector('#reset')
 console.log(businessContainer)
-  
+
 
 function businessCall(businesses) {
-    let businessesDiv = document.createElement('div')
+  let businessesDiv = document.createElement('div')
     businessesDiv.setAttribute("id", "createdDiv")
     console.log(businessesDiv)
     businessesDiv.innerHTML = `</h2> <br> <h3 class="business">${businesses.name} <br> Phone: ${businesses.phone} <br> ${businesses.price} <br> ${businesses.location.address1}, ${businesses.location.city}</h3>`
     if(`${businesses.price}` === undefined){
-      `${businesses.price}` === null
+     `${businesses.price}` === ""
     }
     console.log(businessesDiv)
-      businessContainer.appendChild(businessesDiv)
-}
+    businessContainer.appendChild(businessesDiv)
+  }
 
   const createCall = (e) => {
     e.preventDefault()
@@ -27,19 +27,24 @@ function businessCall(businesses) {
     .then(response => {
       console.log(response.data)
       for (i = 0; i < response.data.businesses.length; i++){
-    businessCall(response.data.businesses[i])
-  }
-  console.log(response.data)
-})
-.catch(function(err) {
-  console.log(err)
-})
+        businessCall(response.data.businesses[i])
+      }
+      console.log(response.data)
+    })
+    .catch(function(err) {
+      console.log(err)
+    })
 }
 
 function clearContainer() {
-businessContainer.innerHTML = ""
+  businessContainer.innerHTML = ""
 }
+const inputText = document.querySelector('#zCode')
 
+// function clearInputText() {
+//   inputText.target.value=""
+// }
 
+// reset.addEventListener('click', clearInputText)
 zCode.addEventListener('submit', createCall);
 reset.addEventListener('click', clearContainer)
